6b74e34cab8d9742799908d81119fa1f6c41f9a0
mrmaleksaifali <maleksaifali786@gmail.com>
2025-07-14 16:50:23 +0530

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const PORT = 4000;
 const db = mysql.createConnection({
   host: 'localhost',
   user: 'nodeuser',
-  password: 'StrongPassword123!',
+  password: '1234',
   database: 'demo_db'
 });
 
